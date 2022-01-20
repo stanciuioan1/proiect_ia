8b196da2d1db5b3c3db496a3a4232959a41cb245
Teodor Anitoaei <anitoaei.teodor@gmail.com>
2022-01-20 23:28:36 +0200

    Working

--- differential_evolution.py
+++ differential_evolution.py
@@ -9,7 +9,7 @@ UPPER_BOUND = 1000
 MUTATION_RATE = 0.02
 CROSSOVER_PROBABILITY = 0.9
 DIFFERENTIAL_FACTOR = 0.8
-MAX_ERROR = 1.e-5
+MAX_ERROR = 1.e-10
 EQUATION = []
 
 
@@ -19,14 +19,6 @@ def evaluateFunction(z: complex):
     for tup in EQUATION:
         if tup[0] == 'poly':
             result += z ** int(tup[3]) * (int(tup[1]) + int(tup[2]) * 1j)
-        elif tup[0] == 'sin':
-            result += cmath.sin(z) * (int(tup[1]) + int(tup[2]) * 1j)
-        elif tup[0] == 'cos':
-            result += cmath.cos(z) * (int(tup[1]) + int(tup[2]) * 1j)
-        elif tup[0] == 'tg':
-            result += cmath.tan(z) * (int(tup[1]) + int(tup[2]) * 1j)
-        elif tup[0] == 'ctg':
-            result += 1/cmath.tan(z) * (int(tup[1]) + int(tup[2]) * 1j)
     return result
 
 def generatePopulation(populationSize=POP_SIZE):
@@ -39,30 +31,22 @@ def generatePopulation(populationSize=POP_SIZE):
 
 
 def getEliteMember(pop):
-    fittest = pop[0]
+    fittest = pop[0].copy()
     fittestValue = abs(evaluateFunction(fittest[0] + fittest[1] * 1j))
     
     for member in pop[1:]:
         memberValue = abs(evaluateFunction(member[0] + member[1] * 1j))
         if fittestValue > memberValue:
             fittestValue = memberValue
-            fittest = member
+            fittest = member.copy()
     
     return fittest
 
-
-def mutate(pop):
-    for member in pop:
-        for i in range(len(member)):
-            if random.randint(0, 100) / 100 < MUTATION_RATE:
-                member[i] = random.randint(LOWER_BOUND, UPPER_BOUND)
-
-
 def getMembersBesides(population, member):
-    a = member
-    b = member
-    c = member
-    while member == a or a == b or b == c:
+    a = member.copy()
+    b = member.copy()
+    c = member.copy()
+    while member == a or member == b or member == c or a == b or b == c or a == c:
         a = population[random.randint(0, len(population) - 1)]
         b = population[random.randint(0, len(population) - 1)]
         c = population[random.randint(0, len(population) - 1)]
@@ -83,23 +67,21 @@ def differentialEvolution(equation, maxEpochs=50000):
         if abs(evaluateFunction(eliteMember[0] + eliteMember[1] * 1j)) < MAX_ERROR:
             return eliteMember
 
-        childrenPop = []
         for j, member in enumerate(population):
             a, b, c = getMembersBesides(population, member)
             newCh = member.copy()
             for i in range(SOLUTION_DIM):
-                mutationProb = random.randint(0, 100) / 100
-                if mutationProb < MUTATION_RATE:
-                    newCh[i] = random.randint(LOWER_BOUND, UPPER_BOUND)
-
                 crossProbability = random.randint(0, 100) / 100
                 if crossProbability < CROSSOVER_PROBABILITY:
                     newCh[i] = a[i] + DIFFERENTIAL_FACTOR * (b[i] - c[i])
 
-            if abs(evaluateFunction(member[0] + member[0] * 1j)) > \
-                abs(evaluateFunction(newCh[0] + newCh[0] * 1j)):
-                population[j] = newCh
+                mutationProb = random.randint(0, 100) / 100
+                if mutationProb < MUTATION_RATE:
+                    newCh[i] = random.randint(LOWER_BOUND, UPPER_BOUND)
+
+            if abs(evaluateFunction(member[0] + member[1] * 1j)) > \
+                abs(evaluateFunction(newCh[0] + newCh[1] * 1j)):
+                population[j] = newCh.copy()
     
     print('Could not find acceptable solution!')
-    population = childrenPop.copy()
     return 0 + 0j
